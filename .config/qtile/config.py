a11482d896fd8082ecc9627fbe9eb82c802e9f39
Justine Smithies <justine@smithies.me.uk>
2021-04-11 19:33:12 +0100

    Moving widgets around.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -289,16 +289,10 @@ screens = [
                     stop_pause_text='',
                     **widget_defaults
                 ),
+                widget.Systray(),
                 widget.GenPollText(update_interval=1, **widget_defaults, func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/volume.sh")).decode()),
                 widget.GenPollText(update_interval=1, **widget_defaults, func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/battery.sh")).decode()),
                 widget.GenPollText(update_interval=1, **widget_defaults, func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/network.sh")).decode(), mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(os.path.expanduser("~/.local/bin/network.sh ShowInfo"), shell=True), 'Button3': lambda: qtile.cmd_spawn(terminal+' -e nmtui', shell=True)}),
-                widget.Systray(),
-                widget.Spacer(length = 10),
-                widget.GenPollText(func=custom_date, update_interval=1, **widget_defaults),
-                widget.Clock(
-					**widget_defaults,
-					format='%H:%M'
-					),
 				widget.Spacer(length = 10),
             ],
             30, margin=[10, 16, 0, 16] # N E S W
