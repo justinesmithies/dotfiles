004f834fc0bd32c5d4d055072a92abe4c3e7f0a3
JustineSmithies <justine@smithies.me.uk>
2021-06-09 18:01:51 +0100

    Tidied up .config/qtile/autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -11,14 +11,6 @@ __kill() { kill -9 "$(pidof "$1")" >/dev/null 2>&1 ; }
 __start() { sleep 1 && "$@" >/dev/null 2>&1 & }
 __running() { pidof "$1" >/dev/null 2>&1 ;}
 
-
-## compositor
-#run-or-restart lxpolkit                                # policy kit
-#run-or-restart jack_control start                      # jack2 audio server
-#run-or-restart light-locker                            # screen locker
-#run-or-restart clipmenud                               # clipboard manager
-#run-or-restart nm-applet 
-
 # Set the wallpaper    
 #feh --bg-fill /home/justine/Pictures/Wallpapers/deer_art_vector_134088_3840x2160.jpg &
 
@@ -56,18 +48,4 @@ fi
 #if cmd_exist nm-applet ; then
     #__kill nm-applet
     #__start nm-applet
-#fi
-
-# Nextcloud
-
-#if cmd_exist nextcloud ; then
-#    __kill nextcloud
-#    __start nextcloud
-#fi
-
-# Telegram
-
-#if cmd_exist telegram-desktop ; then
-    #__kill telegram-desktop
-    #__start telegram-desktop -startintray
 #fi
