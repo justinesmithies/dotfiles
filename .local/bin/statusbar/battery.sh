7236e8d3f96612adb01017f96da6182e9182baea
JustineSmithies <justine@smithies.me.uk>
2021-06-26 22:11:18 +0100

    Removed unwanted spaces from before icons

--- .local/bin/statusbar/battery.sh
+++ .local/bin/statusbar/battery.sh
@@ -3,57 +3,57 @@
 function IconUpdate() {
 if [[ "$(cat /sys/class/power_supply/BAT0/status)" == "Discharging" ]]; then
 	if [[ "$(cat /sys/class/power_supply/BAT0/capacity)" == "100" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "89" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "100" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "79" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "90" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "69" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "80" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "59" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "70" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "49" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "60" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "39" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "50" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "29" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "40" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "19" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "30" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "9" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "20" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "0" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "10" ]]; then
-		icon=" "
+		icon=""
 	fi
 fi
 if [[ "$(cat /sys/class/power_supply/BAT0/status)" == "Charging" ]]; then
 	if [[ "$(cat /sys/class/power_supply/BAT0/capacity)" == "100" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "89" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "100" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "79" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "90" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "69" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "80" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "59" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "70" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "49" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "60" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "39" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "50" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "29" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "40" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "29" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "30" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "9" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "20" ]]; then
-		icon=" "
+		icon=""
 	elif [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -ge "0" && "$(cat /sys/class/power_supply/BAT0/capacity)" -le "10" ]]; then
-		icon=" "
+		icon=""
 	fi
 fi
 if [[ "$(cat /sys/class/power_supply/BAT0/status)" == "Unknown" ]]; then
 	if [[ "$(cat /sys/class/power_supply/BAT0/capacity)" == "100" ]]; then
-		icon=" "
+		icon=""
 	fi
 fi
 capacity="$(cat /sys/class/power_supply/BAT0/capacity)"
